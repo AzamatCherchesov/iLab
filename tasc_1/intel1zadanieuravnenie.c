5d0d400db9bc3df6eca26f7dbd776fbbf2b06bcd
Azamat <a1tt@rambler.ru>
2014-10-01 22:41:48 +0400

    Kvadratnoe_uravnenie

--- tasc_1/intel1zadanieuravnenie.c
+++ tasc_1/intel1zadanieuravnenie.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 #include <math.h>
 #define MISTAKE 10 
+//-------------------------------------- stucts
+
+struct Solution // структура хранит информацию наличия корней ,их количества и численные результаты
+{
+  int number;
+  float solution_1;
+  float solution_2;
+};#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define MISTAKE 10 
 
 // eta programma reshaet kvadratnoe uravnenie , i vivodit kolichestvo korney  , uchitivaet oshibku vvoda bukv 
 
@@ -96,3 +107,91 @@ int Kvadratnoe_uravnenie()
      
      return 0;
 }
+
+//------------------------------------ prototypes
+
+int Kvadratnoe_uravnenie();    //   квадратное уравнение- функция ,возвращающая указатель на стэк, где хранится решение квадратного уравнения
+
+//-------------------------------------  main
+
+int main()
+{	
+	if( Kvadratnoe_uravnenie() == MISTAKE ) printf ( " Pri Vvode proizoshla oshibka!"); 
+	printf("\n");
+	system("PAUSE");
+	return 0;
+}
+
+//----------------------------------- fnctions
+
+int Kvadratnoe_uravnenie()
+{/*
+	char *rptr;
+	char a [ 100 ] , b [ 100 ] , c [ 100 ] ;
+	double rate_1 = 0 , rate_2 = 0 , rate_3 = 0; 
+	struct Solution Result;
+	float diskrim = rate_2*rate_2 - 4*rate_1*rate_3;
+
+
+	printf ("Vvedite znachenie vhodnih parametrov a,b,c.\n V sluchae vvoda nevernih simvolov , programma vidast oshibku\n");
+	scanf ("%s%s%s",a,b,c);
+	//место для проверки на числа
+
+
+	 rate_1 = strtod ( a , &rptr );
+	 if( *rptr != NULL ) return MISTAKE; 
+	 rate_2 = strtod ( b , &rptr );
+	 if( *rptr != NULL) return MISTAKE; 
+	 rate_3 = strtod ( c , &rptr );
+	 if( *rptr != NULL) return MISTAKE; 
+
+
+	//------------------solution
+	if (rate_1 == 0) 
+	   {
+		if (rate_2 == 0)
+           {
+			if (rate_3 == 0) 
+		        Result.number = 3;
+			else    
+				Result.number = 0;
+			}
+		else      
+		    {
+			 Result.number = 1;
+			 Result.solution_1 = (-rate_3/rate_2);
+		     }
+		}
+	 else	
+	    { 
+	     if (diskrim < 0)    
+			 Result.number = 0;
+		 else
+		    {
+			  Result.number = 2;
+		      Result.solution_1     = (-rate_2 - sqrt(diskrim)) / (2 * rate_1);
+			  Result.solution_2     = (-rate_2 + sqrt(diskrim)) / (2 * rate_1);
+			}
+	      }
+	//-------------------solution
+
+	//------------------printing
+	if (Result.number == 0) 
+       printf("Korney net");
+
+	if (Result.number == 3) 
+       printf("Korney beskonechno mnogo");
+	
+	if (Result.number == 1) 
+       printf("the number of solutions is 1 \n solution = %f", Result.solution_1);
+	
+	if (Result.number == 2) 
+       printf("the number of solutions is 2 \n first solution = %f \n second solution = %f", Result.solution_1, Result.solution_2);
+	*/
+	return 0;
+}
+
+
+
+
+
